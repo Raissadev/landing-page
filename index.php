243a73146dad909a3864b20700ce5c7872e6c6c4
Raissadev <82960240+Raissadev@users.noreply.github.com>
2021-08-24 20:56:37 -0300

    main commit

--- index.php
+++ index.php
@@ -128,11 +128,9 @@
         include('models/footer.php');
     ?>
 
-
-
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="js/functions.js"></script>
-
+    
 </body>
 </html>
 
